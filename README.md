aa760f5bd91175ecee94af0366239e9052280495
Joe Doss <joe@solidadmin.com>
2022-05-04 12:47:30 -0500

    Mmmmm PDFs...

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ppngx
-Podman + Paperless NGX
+# ppngx: Podman + Paperless-ngx
+
+This is a quick script for setting up [Paperless-ngx](https://github.com/paperless-ngx/paperless-ngx) with [Rootless Podman](https://github.com/containers/podman/blob/main/docs/tutorials/rootless_tutorial.md) inside a [Podman Pod](https://docs.podman.io/en/latest/markdown/podman-pod.1.html).
+
+It will launch Redis, PostgreSQL, Tika, Gotenberg, SFTPGo and Paperless-ngx inside a self-contained pod. I got this working with [Brother ADS2800w](https://www.brother-usa.com/products/ads2800w) and SFTP to SFTPGo. Good luck!
+
+# License
+
+MIT License
+
+Copyright (c) 2022 Joe Doss
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ start.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+set -e
+
+PAPERLESS_PORT=8000
+PAPERLESS_UID=1000
+PAPERLESS_GID=1000
+
+SFTPGO_SFTP_PORT=2022
+SFTPGO_HTTP_PORT=8022
+SFTPGO_ADMIN_USER=sftpadmin
+SFTPGO_ADMIN_PASSWORD=supersecret
+SFTPGO_PAPERLESS_USER=myscanner
+SFTPGO_PAPERLESS_PASSWORD=anothersupersecret
+
+PAPERLESS_SECRET_KEY=chamgemechamgemechamgemechamgemechamgemechamgemechamgemechamgeme
+
+REDIS_VERSION=6
+REDIS_PORT=6379
+
+POSTGRESQL_VERSION=13
+POSTGRESQL_PORT=5432
+POSTGRES_USER=paperless
+POSTGRESQL_DB=paperless
+POSTGRESQL_PASSWORD=paperlesschangeme
+
+echo "Creating Paperless Pod..."
+podman pod create --replace --name paperless \
+  -p ${PAPERLESS_PORT}:${PAPERLESS_PORT} \
+  -p ${SFTPGO_SFTP_PORT}:${SFTPGO_SFTP_PORT} \
+  -p ${SFTPGO_HTTP_PORT}:${SFTPGO_HTTP_PORT}
+
+echo "Starting Redis..."
+podman volume create paperless-redis 2> /dev/null ||:
+podman create --replace --pod paperless \
+  --restart=unless-stopped \
+  --name paperless-redis \
+  --volume paperless-redis:/data:Z \
+  docker.io/library/redis:${REDIS_VERSION}
+podman start paperless-redis
+
+echo "Starting PostgreSQL..."
+podman volume create paperless-postgresql 2> /dev/null ||:
+podman create --replace --pod paperless \
+  --restart=unless-stopped \
+  --name paperless-postgresql \
+  --expose ${POSTGRESQL_PORT} \
+  -e POSTGRES_USER=${POSTGRES_USER} \
+  -e POSTGRES_PASSWORD=${POSTGRESQL_PASSWORD} \
+  --volume paperless-postgresql:/var/lib/postgresql/data:Z \
+  docker.io/library/postgres:${POSTGRESQL_VERSION}
+podman start paperless-postgresql
+
+echo "Starting Gotenberg..."
+podman create --replace --pod paperless \
+  --restart=unless-stopped \
+  --name paperless-gotenberg \
+  -e CHROMIUM_DISABLE_ROUTES=1 \
+  docker.io/gotenberg/gotenberg:7
+podman start paperless-gotenberg
+
+echo "Starting Tika..."
+podman create --replace --pod paperless \
+  --restart=unless-stopped \
+  --name paperless-tika \
+  docker.io/apache/tika
+podman start paperless-tika
+
+echo "Starting Paperless..."
+podman create --replace --pod paperless \
+  --name paperless-webserver \
+  --restart=unless-stopped \
+  --stop-timeout=90 \
+  --health-cmd='["curl", "-f", "http://localhost:8000"]' \
+  --health-retries=5 \
+  --health-start-period=60s \
+  --health-timeout=10s \
+  -e PAPERLESS_REDIS=redis://localhost:${REDIS_PORT} \
+  -e PAPERLESS_DBHOST=localhost \
+  -e PAPERLESS_DBNAME=${POSTGRES_USER} \
+  -e PAPERLESS_DBPASS=${POSTGRESQL_PASSWORD} \
+  -e PAPERLESS_TIKA_ENABLED=1 \
+  -e PAPERLESS_TIKA_GOTENBERG_ENDPOINT=http://localhost:3000 \
+  -e PAPERLESS_TIKA_ENDPOINT=http://localhost:9998 \
+  -e USERMAP_UID=${PAPERLESS_UID} \
+  -e USERMAP_GID=${PAPERLESS_GID} \
+  -e PAPERLESS_SECRET_KEY=${PAPERLESS_SECRET_KEY} \
+  -e PAPERLESS_TIME_ZONE=America/Chicago \
+  -e PAPERLESS_OCR_LANGUAGE=eng \
+  -v paperless-data:/usr/src/paperless/data:Z \
+  -v paperless-media:/usr/src/paperless/media:Z \
+  -v paperless-consume:/usr/src/paperless/consume:U,z \
+  -v ${PWD}/export:/usr/src/paperless/export:U,Z \
+  ghcr.io/paperless-ngx/paperless-ngx:latest
+podman start paperless-webserver
+
+echo "Starting SFTPGo..."
+podman create --replace --pod paperless \
+  --restart=unless-stopped \
+  --name paperless-sftpgo \
+  -e SFTPGO_DATA_PROVIDER__CREATE_DEFAULT_ADMIN=1 \
+  -e SFTPGO_DEFAULT_ADMIN_USERNAME=${SFTPGO_ADMIN_USER} \
+  -e SFTPGO_DEFAULT_ADMIN_PASSWORD=${SFTPGO_ADMIN_PASSWORD} \
+  -e SFTPGO_HTTPD__BINDINGS__0__PORT=${SFTPGO_HTTP_PORT} \
+  -v paperless-sftpgo:/var/lib/sftpgo:Z \
+  -v paperless-consume:/opt/paperless/consume:rw,z \
+  ghcr.io/drakkan/sftpgo:v2
+podman start paperless-sftpgo
+
+sleep 5
+
+JWT=$(curl -s -u ${SFTPGO_ADMIN_USER}:${SFTPGO_ADMIN_PASSWORD} http://127.0.0.1:${SFTPGO_HTTP_PORT}/api/v2/token | jq -r '.access_token')
+
+curl -s --header "Content-Type: application/json" \
+     --header 'Accept: application/json' -H "Authorization: Bearer ${JWT}" \
+     --request POST \
+     --data '{"username": "'${SFTPGO_PAPERLESS_USER}'", "password": "'${SFTPGO_PAPERLESS_PASSWORD}'", "status": 1, "home_dir": "/opt/paperless/consume", "permissions": {"/": ["*"]}}' \
+     http://127.0.0.1:${SFTPGO_HTTP_PORT}/api/v2/users
+
+SFTP_PUBLIC_KEY=$(podman exec -it paperless-sftpgo cat /var/lib/sftpgo/id_rsa.pub)
+
+echo "Add the SFTP Public Key to your scanner:"
+echo "${SFTP_PUBLIC_KEY}"
+
+echo "${SFTP_PUBLIC_KEY}" > ${PWD}/sftp_rsa_host_key.pub
